e53d0963a6d709dad92a4825fcbce87c9eca4792
ab3al <abzal_amanzhol_94@mail.ru>
2018-11-10 15:31:50 +0600

    prim2 Δ(t)

--- prim.sql
+++ prim.sql
@@ -1,18 +1,21 @@
 
+-- Структура таблицы `prim`
+--
 
 CREATE TABLE `prim` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `fnc` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+  `fnc` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `form` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Дамп данных таблицы `prim`
 --
 
-INSERT INTO `prim` (`id`, `name`, `fnc`) VALUES
-(1, 'Есеп 1', 'calcPrim1()'),
-(2, 'Есеп 2', 'calcPrim2()');
+INSERT INTO `prim` (`id`, `name`, `fnc`, `form`) VALUES
+  (1, 'Есеп 1', 'calcPrim1()', '<p>1 коэфицент: <input type=\"number\" id=\"n1\" value=\"2\"></p>                 <p>2 коэфицент: <input type=\"number\" id=\"n2\" value=\"-3\"></p>'),
+  (2, 'Есеп 2', 'calcPrim2()', '<p>1 коэфицент: <input type=\"number\" id=\"n1\" value=\"2\"></p>                 <p>2 коэфицент: <input type=\"number\" id=\"n2\" value=\"-3\"></p>');
 
 --
 -- Индексы сохранённых таблиц
@@ -36,6 +39,5 @@ ALTER TABLE `prim`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
 
 ALTER TABLE `lecture` ADD `prim` INT NOT NULL AFTER `test`;
