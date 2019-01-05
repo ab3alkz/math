ba8b3fe1a765dc2fe9577a35c1ce05bfc14df229
abzal <a.amanzhol@enbek.kz>
2019-01-05 23:37:25 +0600

    prim3

--- prim.sql
+++ prim.sql
@@ -1,43 +1,7 @@
 
--- Структура таблицы `prim`
---
-
-CREATE TABLE `prim` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `fnc` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `form` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Дамп данных таблицы `prim`
---
+delete  from prim;
 
 INSERT INTO `prim` (`id`, `name`, `fnc`, `form`) VALUES
-  (1, 'Есеп 1', 'calcPrim1()', '<p>1 коэфицент: <input type=\"number\" id=\"n1\" value=\"2\"></p><p>2 коэфицент: <input type=\"number\" id=\"n2\" value=\"-3\"></p>'),
-  (2, 'Есеп 2', 'calcPrim2()', '<p>1 коэфицент: <input type=\"number\" id=\"n1\" value=\"2\"></p><p>2 коэфицент: <input type=\"number\" id=\"n2\" value=\"-3\"></p><p>Функция:<textarea style=\"width: 100%; height: 60px\" class=\"mceEditor\" id=\"functionArea\" > </textarea></p>');
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `prim`
---
-ALTER TABLE `prim`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `prim`
---
-ALTER TABLE `prim`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-ALTER TABLE `lecture` ADD `prim` INT NOT NULL AFTER `test`;
+(1, 'Есеп 1', 'calcPrim1()', '<script>\n                        window.onload = function (ev) {\n                            startPrim1(\'prim1\');\n                        }\n                    </script><p>1 коэфицент: <input type=\"number\" id=\"n1\" value=\"\"></p>                 <p>2 коэфицент: <input type=\"number\" id=\"n2\" value=\"\"></p>'),
+(2, 'Есеп 2', 'calcPrim2()', '<script>\n                        window.onload = function (ev) {\n                            startPrim2(\'prim2\');\n                        }\n                    </script><p>1 коэфицент: <input type=\"number\" id=\"n1\" value=\"\"></p><p>2 коэфицент: <input type=\"number\" id=\"n2\" value=\"\"></p><p>Функция:<textarea style=\"width: 100%; height: 60px\" class=\"mceEditor\" id=\"functionArea\" > </textarea></p>'),
+(3, 'Есеп 3', 'calcPrim3(\'prim3\')', '<script>\n                        window.onload = function (ev) {\n                            startPrim3(\'prim3\'); \n                        }\n                    </script><div class=\"math-frame\">\n                        <div class=\"math-inner\">\n                            <div class=\"math-main\">p</div>\n                            <div class=\"math-index\">1</div>\n                            <div class=\"math-main\">&nbsp;&nbsp;&nbsp;=<input style=\"margin-left: 37px\" type=\"number\"\n                                                                             id=\"p1\" value=\"\"></div>\n                        </div>\n                    </div>\n\n                    <div class=\"math-frame\">\n                        <div class=\"math-inner\">\n                            <div class=\"math-main\">p</div>\n                            <div class=\"math-index\">2</div>\n                            <div class=\"math-main\">&nbsp;&nbsp;&nbsp;=<input style=\"margin-left: 37px\" type=\"number\"\n                                                                             id=\"p2\" value=\"\"></div>\n                        </div>\n                    </div>\n\n                    <div class=\"math-frame\">\n                        <div class=\"math-inner\">\n                            <div class=\"math-main\">h(0)&nbsp;&nbsp;&nbsp;=<input style=\"margin-left: 20px\" type=\"number\"\n                                                                                 id=\"h0\" value=\"\"></div>\n                        </div>\n                    </div>\n                    <div class=\"math-frame\">\n                        <div class=\"math-inner\">\n                            <div class=\"math-main\">h\'(0)&nbsp;&nbsp;&nbsp;=<input style=\"margin-left: 14px\"\n                                                                                  type=\"number\" id=\"h_0\" value=\"\">\n                            </div>\n                        </div>\n                    </div>\n                    <div class=\"math-frame\">\n                        <div class=\"math-inner\">\n                            <div class=\"math-main\">[y]</div>\n                            <div class=\"math-index\">\n                                <div class=\"math-fraction\">\n                                    <table>\n                                        <tbody>\n                                        <tr class=\"math-fraction-top\">\n                                            <td>1</td>\n                                        </tr>\n                                        <tr>\n                                            <td class=\"math-fraction-bottom\">2</td>\n                                        </tr>\n                                        </tbody>\n                                    </table>\n                                </div>\n                            </div>\n                            <div class=\"math-main\">\n                                &nbsp;&nbsp;&nbsp;=<input style=\"margin-left: 14px\"\n                                                      type=\"number\" id=\"y05\" value=\"\" onchange=\"onchange05()\">\n                            </div>\n                        </div>\n                    </div>\n                    <div class=\"math-frame\">\n                        <div class=\"math-inner\">\n                            <div class=\"math-main\">[y\']</div>\n                            <div class=\"math-index\">\n                                <div class=\"math-fraction\">\n                                    <table>\n                                        <tbody>\n                                        <tr class=\"math-fraction-top\">\n                                            <td>1</td>\n                                        </tr>\n                                        <tr>\n                                            <td class=\"math-fraction-bottom\">2</td>\n                                        </tr>\n                                        </tbody>\n                                    </table>\n                                </div>\n                            </div>\n                            <div class=\"math-main\">\n                                &nbsp;&nbsp;&nbsp;=<input style=\"margin-left: 14px\"\n                                                          type=\"number\" id=\"y_05\" value=\"\" onchange=\"onchange05()\">\n                            </div>\n                        </div>\n                    </div>');
